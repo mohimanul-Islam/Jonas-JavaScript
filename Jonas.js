fe33ae979af990142d03e4f083e2a251cc4af387
Mohimanul2024 <emondm2024@gmail.com>
2024-06-12 16:55:31 +0600

    update4

--- Jonas.js
+++ Jonas.js
@@ -1,4 +1,4 @@
-let js = 'Amazing';
+/*let js = 'Amazing';
 console.log(20 + 42 + 30 - 12);
 console.log('Emon');
 console.log(23);
@@ -8,3 +8,135 @@ console.log(name);
 console.log(name);
 let myFirstJob = 'freelancer';
 console.log(myFirstJob);
+*/
+
+// let javascript = true;
+// console.log(javascript);
+// console.log(typeof true);
+// console.log(typeof javascript);
+// console.log(typeof 10);
+// console.log(typeof 'Emon');
+
+// javascript = 'fun';
+// console.log(typeof javascript);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+// year = 2022;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// const sara = 23;
+// console.log(sara);
+// const now = 2055;
+// const sara = now - 2024;
+// const emon = now - 2020;
+// console.log(sara, emon);
+// console.log(sara * 2, emon / 10, 2 ** 3);
+
+// let frstName = 'Emon';
+// let lastName = 'Islam';
+// console.log(frstName+' '+lastName);
+
+//assignment operator
+// let x=25+20;
+// console.log(x);
+// x+=10;
+// console.log(x);
+// x*=2;
+// console.log(x);
+// x++;
+// console.log(x);
+// x--;
+// console.log(x);
+
+//comparison operator
+//  sara = 25;
+// let jon = 36;
+// console.log(sara > jon);
+// console.log(jon > sara);
+// console.log(now-2024 < now-2020);
+
+// let x, y;
+// x=y=20+30-10;
+// console.log(x, y);
+
+// const ageav = (sara+emon)/2;
+// console.log(sara, emon, ageav);
+
+// const massMark=78, heightMark=1.69, massJohn=92, heightJohn=1.95;
+
+// const BMIMark=massMark/(heightMark*heightMark)
+// const BMIJohn=massJohn/(heightJohn*heightJohn)
+// console.log(BMIMark,BMIJohn);
+// const markHigherBMI = (BMIMark>BMIJohn);
+// console.log(markHigherBMI);
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+// const BMIMark = massMark / heightMark**2;
+// const BMIJohn = massJohn/(heightJohn*heightJohn);
+// console.log(BMIMark, BMIJohn);
+// const markHeigherBMI=BMIMark>BMIJohn;
+// console.log(markHeigherBMI);
+
+// const frstName = 'Emon';
+// const job = 'Freelancer';
+// const birth = 2000;
+// const year = 2024;
+
+// const emon = "I'm "+frstName+', a '+(year-birth) +' years old '+job+'!';
+// console.log(emon);
+
+// const emonNew = `I'm ${frstName}, a ${year-birth} years old ${job}!`;
+// console.log(emonNew);
+
+// console.log(`My name is Emon`);
+// console.log(`string with 
+//     mltiple 
+//     line`);
+
+// const age = 15;
+// if(age >=18){
+//     console.log("Emon can start driving license");
+// }else{
+//     const yearsLeft = 18-age;
+//     console.log(`Emon cannot drive now, wait untill ${yearsLeft} years`);
+// }
+
+// const birthYear = 1998;
+// let century;
+// if(birthYear<=1999){
+//     // console.log("You born in 20th century");
+//     century=20;
+// }else{
+//     century=21;
+//     // console.log("You born in 21th century");
+// }
+// console.log(century);
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+// const BMIMark = massMark / heightMark**2;
+// const BMIJohn = massJohn/(heightJohn*heightJohn);
+// console.log(BMIMark, BMIJohn);
+// const markHeigherBMI=BMIMark>BMIJohn;
+// console.log(markHeigherBMI);
+
+// if(BMIMark>BMIJohn){
+//     console.log(`Mark's BMI ${BMIMark} higher than Johns ${BMIJohn}!`);
+// }else{
+//     console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark} !`);
+// }
+
