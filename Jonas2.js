d9eefbe6ee0160919541872693428fd3763e7eab
Mohimanul2024 <emondm2024@gmail.com>
2024-06-13 12:04:48 +0600

    newfilemod

--- Jonas2.js
+++ Jonas2.js
@@ -8,3 +8,22 @@
 
 //functions
 
+function logger (){
+    console.log('my name is Emon');
+}
+//calling running or invocing function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, orranges) {
+    console.log(apples, orranges);
+    const juice = `juice with ${apples} apples and ${orranges} orenges. `;
+    return juice;
+}
+const appleJuice = fruitProcessor(5,2);
+console.log(appleJuice);
+const appleOrangeJuice = fruitProcessor(4,3);
+console.log(appleOrangeJuice);
+console.log(fruitProcessor(5,3));
+
