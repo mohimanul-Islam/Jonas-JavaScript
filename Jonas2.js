b76793a42c1fb75b9b7fc7ea65ce89329062ab04
Mohimanul2024 <emondm2024@gmail.com>
2024-06-13 22:13:18 +0600

    newfilemod3

--- Jonas2.js
+++ Jonas2.js
@@ -60,7 +60,46 @@
 
 //practice
 
-function log(){
-    console.log("my name is Emon");
-}
-log();
+// function name(){
+//     console.log("my Name is Emon");
+// }
+// name();
+// ///function declaretion
+// function calcAge(birthyear){
+//     return 2037-birthyear;
+// }
+// console.log(calcAge(2008));
+// const age1 = calcAge(2000);
+// console.log(age1);
+// const age2 = calcAge(1995);
+// console.log(age2);
+
+// //function expresion
+// const age3 = function(birthyear){
+//     return 2038-birthyear;
+// }
+// const year = age3(1998);
+// console.log(year);
+
+///arrow function
+
+// const emon = year => 2038-year;
+// const age= emon(2024)
+// console.log(age);
+
+// const yearUntilRetirement = (birthyear, name) => {
+//     const age = 2039-birthyear;
+//     const retirement = 65-age;
+//     return `${name} will retire in ${retirement} years.`;
+// }
+// console.log(yearUntilRetirement(2000, 'Emon')); 
+// console.log(yearUntilRetirement(1995, 'Johnny')); 
+
+// const yearofretirement = (birthYear,name) =>{
+//     const age = 2038-birthYear;
+//     const retirement = 65-age;
+//     return `${name} will retire in ${retirement} years`; 
+// }
+// console.log(yearofretirement(2000, 'Emon')); 
+// console.log(yearofretirement(1982, 'Jonas')); 
+
