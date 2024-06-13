df0576f6d176227ef1ae26eba23844045eb902e1
Mohimanul2024 <emondm2024@gmail.com>
2024-06-13 14:10:56 +0600

    newfilemod1

--- Jonas2.js
+++ Jonas2.js
@@ -8,22 +8,45 @@
 
 //functions
 
-function logger (){
-    console.log('my name is Emon');
-}
-//calling running or invocing function
-logger();
-logger();
-logger();
+// function logger (){
+//     console.log('my name is Emon');
+// }
+// //calling running or invocing function
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples, orranges) {
-    console.log(apples, orranges);
-    const juice = `juice with ${apples} apples and ${orranges} orenges. `;
-    return juice;
-}
-const appleJuice = fruitProcessor(5,2);
-console.log(appleJuice);
-const appleOrangeJuice = fruitProcessor(4,3);
-console.log(appleOrangeJuice);
-console.log(fruitProcessor(5,3));
+// function fruitProcessor(apples, orranges) {
+//     console.log(apples, orranges);
+//     const juice = `juice with ${apples} apples and ${orranges} orenges. `;
+//     return juice;
+// }
+// const appleJuice = fruitProcessor(5,2);
+// console.log(appleJuice);
+// const appleOrangeJuice = fruitProcessor(4,3);
+// console.log(appleOrangeJuice);
+// console.log(fruitProcessor(5,3));
+//function declaration
+// function age(birthyear) {
+//   return 2025 - birthyear;
+// }
+// const age1 = age(1998);
+// //function expression
+// const age2 = function (birthyear) {
+//   return 2035 - birthyear;
+// };
+// const age3 = age2(1995);
+// console.log(age3, age1);
 
+//arrow function
+
+// const calcage = Byear => 2035-Byear;
+// const age = calcage(2000);
+// console.log(age);
+
+const retayermentyear = birthday => {
+  const age = 2035 - birthday;
+  const retirement = 65 - age;
+  return retirement;
+};
+console.log(retayermentyear(1991));
